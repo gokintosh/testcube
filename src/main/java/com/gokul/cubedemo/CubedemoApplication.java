c8838927d8ade60a6b08c6b5017617bbfdb3607a
gokintosh <gokul.kannambally_balachandran@student.po.edu.pl>
2022-11-23 12:29:41 +0100

    jdsgfj

---
+++ src/main/java/com/gokul/cubedemo/CubedemoApplication.java
@@ -0,0 +1,13 @@
+package com.gokul.cubedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CubedemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CubedemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gokul/cubedemo/CubedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gokul.cubedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CubedemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
